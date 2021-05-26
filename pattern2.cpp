82bbc5fbf1516495b27c301057f8d91d0304829a
srinu1085 <84057026+srinu1085@users.noreply.github.com>
2021-05-26 14:02:11 +0530

    Create pattern2.cpp

---
+++ pattern2.cpp
@@ -0,0 +1,23 @@
+/* printing 
+11111
+22222
+33333
+44444
+55555
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n;
+  for(int i=1;i<=n;i++)
+  {
+    for(int j=1;j<=n;j++)
+    {
+      cout<<i;
+    }
+    cout<<endl;
+  }
+}
