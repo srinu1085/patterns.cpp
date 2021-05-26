61fe4399fafed591c4e076cf9595210b8532d820
srinu1085 <84057026+srinu1085@users.noreply.github.com>
2021-05-26 14:29:23 +0530

    Create pattern3_3.cpp

---
+++ pattern3_3.cpp
@@ -0,0 +1,24 @@
+/* printing
+eg:
+54321
+54321
+54321
+54321
+54321
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n;
+  for(int i=1;i<=n;i++)
+  {
+    for(int j=n;j>=1;j--)
+    {
+      cout<<j;
+    }
+    cout<<endl;
+  }
+}
