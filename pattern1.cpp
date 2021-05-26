16ba9923c4d392110900951b4f7b3a3e85d77477
srinu1085 <84057026+srinu1085@users.noreply.github.com>
2021-05-26 13:56:21 +0530

    Create pattern1.cpp

---
+++ pattern1.cpp
@@ -0,0 +1,23 @@
+/* To print
+
+*****
+*****
+*****
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int i,j;
+for(i=0;i<3;i++)
+{
+for(j=0;j<5;j++)
+{
+cout<<"*";
+}
+cout<<endl;
+}
+
+}
