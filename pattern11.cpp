7472ef267e44d8aacbb1ee0ea6b0322a2b1b62ef
srinu1085 <84057026+srinu1085@users.noreply.github.com>
2021-05-27 08:38:36 +0530

    Update pattern11.cpp

--- pattern11.cpp
+++ pattern11.cpp
@@ -1,3 +1,27 @@
+/* To print
+eg:
+*****
+*****
+*****
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int i,j,n,m;
+  cin>>n>>m;
+  for(i=0;i<n;i++)//no. of rows
+  {
+    for(j=0;j<m;j++)//no. of coloumns 
+    {
+      cout<<"*";
+    }
+  cout<<endl;
+  }
+}
+
+
 /* rprinting
 eg:
 *****
