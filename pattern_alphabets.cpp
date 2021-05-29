8e78fce2da1d25d48b1fd86fede4d7c5f973a476
srinu1085 <84057026+srinu1085@users.noreply.github.com>
2021-05-29 19:44:15 +0530

    Create pattern_alphabets.cpp

---
+++ pattern_alphabets.cpp
@@ -0,0 +1,47 @@
+/* printing
+eg:
+* * * 
+*
+* * *
+*
+* * *
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n;
+  if(n%2!=0)
+  {
+    for(int i=1;i<=n;i++)
+    {
+      if(i==1 || i==(n/2)+1 || i==n)
+      {
+        for(int j=1;j<=(n/2)+1;j++)
+        {
+          cout<<"* ";
+        }
+      }
+      else if(i>1 || i<=(n/2)+1)
+      {
+        cout<<"*";
+      }
+      else
+      {
+        for(int k=n;k=n;k++)
+        { 
+          cout<<"*";
+        }
+      } 
+      cout<<endl;
+    }
+  }
+  else
+  {
+    cout<<"enter odd number to get E";
+  }
+  return 0;
+}
