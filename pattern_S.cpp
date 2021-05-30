a3fd211965c4184fa8f692ecd3e71212f8a66297
srinu1085 <84057026+srinu1085@users.noreply.github.com>
2021-05-30 08:18:54 +0530

    Create pattern_S.cpp

---
+++ pattern_S.cpp
@@ -0,0 +1,40 @@
+/* printing 
+eg:
+* * * * 
+*
+*
+* * * *
+
+
+* * * *
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n;
+  if(n%2!=0)
+  {
+    for(int i=1;i<=n;i++)
+    {
+      if(i==1 || i==(n/2)+1 || i==n)
+      {
+        for(int j=1;j<=(n/2)+1;j++)
+        {
+          cout<<"* ";
+        }
+      }
+      else if(i>1 && i<=(n/2))
+      {
+        cout<<"*";
+      }
+      cout<<endl;
+    }
+  }
+  else
+  {
+    cout<<"enter odd number to get 'S'";
+  }
+  return 0;
+}
