6422c53a7e4b8ca4fc8533a412504e9a191f20be
srinu1085 <84057026+srinu1085@users.noreply.github.com>
2021-10-15 12:34:20 +0530

    Update patterns.cpp

--- patterns.cpp
+++ patterns.cpp
@@ -2436,7 +2436,45 @@ int main()
 
 
 /* printing 
-eg:
+eg:* * * * * 
+   *       * 
+   * * * * * 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    //int a[n],b[m];
+    for(int i=1 ;i<=n ;i++)
+    {
+        if(i==1 || i==n)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                cout<<"* ";
+            }
+        }
+        else
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(j==1 || j==m)
+                {
+                    cout<<"* ";   
+                }
+                else
+                {
+                    cout<<"  ";   
+                }
+            }
+        }
+        cout<<endl;
+    }
+}
+
 
 
 
